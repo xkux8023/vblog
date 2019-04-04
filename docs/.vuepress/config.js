a22e7fc7531dd38dc0d0dd5a1d842ce18b230b1c
xkux8023 <326712928@qq.com>
2019-04-04 21:15:49 +0800

    vuepress博客项目结构初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  title: '阿咏个人站',
+  descroption: 'Talk is cheap, show me the code!',
+  head: [
+    ['link', { rel: 'icon', href: '/code.ico' }]
+  ],
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Vue', link: '/vue/' },
+      { text: 'Node', link: '/node/' },
+      { text: 'React', link: '/react/' },
+      { text: 'JavaScript', link: '/javascript/' },
+      { text: 'Github', link: 'https://github.com/xkux8023' },
+    ],
+    sidebar: {
+      '/vue/': [
+        '',
+        'vue_1',
+        'vue_2'
+      ],
+      '/react/': [
+        '',
+        'react_1',
+        'react_2'
+      ],
+      '/node/': [
+        '',
+        'node_1',
+        'node_2'
+      ],
+      '/javascript/': [
+        '',
+        'js_1',
+        'js_2'
+      ]
+    },
+    sidebarDepth: 2,
+    lastUpdated: 'Last Updated'
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,17 @@
+---
+home: true
+heroImage: /avatar.png
+footer: MIT Licensed | Copyright © 2018-present Evan You
+---
+
+
+
+``` js{4}
+export default {
+  data () {
+    return {
+      msg: 'Highlighted!'
+    }
+  }
+}
+```
